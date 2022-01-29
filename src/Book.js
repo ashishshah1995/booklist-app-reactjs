6590d788b02bb11559d988ee07e827f1d927df75
Ashish <shahashish991@gmail.com>
2022-01-29 16:29:44 -0500

    Add Hide/Show functionality

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./index.css";
 
-const Book = ({ img, title, author, description }) => {
+const Book = ({
+  img,
+  title,
+  author,
+  description,
+  id,
+  removeIndividualBook,
+}) => {
   const clickHandler = () => {
     alert(description);
   };
 
+  // Displays Price
   const [price, setPrice] = useState(0);
   const viewPrice = () => {
     if (price === 0) {
@@ -15,16 +23,20 @@ const Book = ({ img, title, author, description }) => {
     }
   };
 
+  // Incease Quantity
   const [quantity, setQuantity] = useState(0);
   const increaseQuantity = () => {
     setQuantity(quantity + 1);
   };
+
+  // Decrease Quantity
   const decreaseQuantity = () => {
     if (quantity > 0) {
       setQuantity(quantity - 1);
     }
   };
 
+  // Reset Quantity
   const resetQuantity = () => {
     setQuantity(0);
   };
@@ -39,30 +51,36 @@ const Book = ({ img, title, author, description }) => {
       <img src={img} alt="" />
       <h1 onClick={() => console.log(description)}>{title}</h1>
       <h4>{author}</h4>
-      <button type="button" class="button button1" onClick={clickHandler}>
+      <button type="button" className="button button1" onClick={clickHandler}>
         Description
       </button>
       <div className="price">
-        <button type="button" class="button button1" onClick={viewPrice}>
+        <button type="button" className="button button1" onClick={viewPrice}>
           {price === 0 ? "View Price" : "Hide Price"}
         </button>
         <h4>{price > 0 ? <h2 className="priceValue">= ${price}</h2> : null}</h4>
       </div>
-
+      <button
+        className="button"
+        style={{ height: "20px", width: "131" }}
+        onClick={() => removeIndividualBook(id)}
+      >
+        Remove book
+      </button>
       <div className="quantity">
         <h3>Quantity</h3>
-        <button class="button button1" onClick={increaseQuantity}>
+        <button className="button button1" onClick={increaseQuantity}>
           {"+"}
         </button>
         <button
-          class="button button1"
+          className="button button1"
           id="decreaseQuantity"
           onClick={decreaseQuantity}
         >
           {"--"}
         </button>
         <button
-          class="button button1"
+          className="button button1"
           style={{ width: "70px", height: "50px" }}
           onClick={resetQuantity}
         >
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
@@ -6,11 +6,70 @@ import { books } from "./books";
 import Book from "./Book";
 
 function BookList() {
+  const [booksData, setBooksData] = useState(books);
+
+  // Removes all books
+  const clearBooks = () => {
+    setBooksData([]);
+  };
+
+  // Shows all Book
+  const handleShowBook = () => {
+    setBooksData(books);
+  };
+
+  // Removes Individual book
+  const removeIndividualBook = (id) => {
+    const updatedBookData = booksData.filter((book) => {
+      return book.id !== id;
+    });
+    setBooksData(updatedBookData);
+  };
+
   return (
-    <section className="booklist">
-      {books.map((book, index) => {
-        return <Book key={index} {...book}></Book>;
-      })}
+    <section className="wrapper">
+      <h1 className="booktitle">Booklist</h1>
+
+      <div className="booklist">
+        {booksData.map((book, index) => {
+          return (
+            <Book
+              key={index}
+              {...book}
+              removeIndividualBook={removeIndividualBook}
+            ></Book>
+          );
+        })}
+      </div>
+      {booksData.length === 0 ? (
+        <div>
+          <h1 style={{ marginTop: "-68px", marginLeft: "388px" }}>
+            No books left!
+          </h1>
+          <button
+            style={{
+              width: "122px",
+              height: "49px",
+              marginLeft: "391px",
+              marginTop: "20px",
+            }}
+            className="button button1"
+            onClick={handleShowBook}
+          >
+            Show Books
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button
+            style={{ marginLeft: "190px" }}
+            className="button button1"
+            onClick={clearBooks}
+          >
+            Hide Books
+          </button>
+        </div>
+      )}
     </section>
   );
 }
